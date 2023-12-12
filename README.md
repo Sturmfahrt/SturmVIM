cc33eeea831a4f860236b0944dfe29b6081175d9
Sturmfahrt <cyberhead81@icloud.com>
2023-12-12 17:00:17 -0500

    Pre package manager commit
    pretty much as basic as it gets. just a few config options and a keybinding.

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # nvim
 This is my active work for a neovim configuration. the current emphasis is that it works well for Rust and LUA.
+
+This is currently based off of the Neovim Configuration tutorial series by typecraft on youtube. i will figure out how to make that a proper link later.
+https://www.youtube.com/playlist?list=PLsz00TDipIffxsNXSkskknolKShdbcALR
+
---
+++ lua/core/keymaps.lua
@@ -0,0 +1,18 @@
+vim.g.mapleader = ' '
+vim.g.maplocalleader = ' '
+
+vim.opt.backspace = '2'
+vim.opt.showcmd = true
+vim.opt.laststatus = 2
+vim.opt.autowrite = true
+vim.opt.cursorline = false
+vim.opt.autoread = true
+vim.wo.relativenumber = true
+
+-- use spaces for tabs and whatnot
+vim.opt.tabstop = 2
+vim.opt.shiftwidth = 2
+vim.opt.shiftround = true
+vim.opt.expandtab = true
+
+vim.keymap.set('n', '<leader>h', ':nohlsearch<CR>')
