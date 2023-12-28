848348d224bfb89990b2c1296f751ec988abd2a3
Sturmfahrt <cyberhead81@icloud.com>
2023-12-28 02:18:58 -0500

    added good syntax hightlighting there is now actual syntax highlighting. this has been enabled by the use of nvim-treesitter. i am not using treesitter for anything else at the moment.

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -12,4 +12,7 @@ return {
     'nvim-telescope/telescope.nvim', branch = '0.1.x',
     dependencies = { 'nvim-lua/plenary.nvim' },
   },
+  {
+    'nvim-treesitter/nvim-treesitter',
+  },
 }
---
+++ lua/user/treesitter.lua
@@ -0,0 +1,9 @@
+require'nvim-treesitter.configs'.setup {
+  ensure_installed = {
+    "lua",
+  },
+  auto_install = true,
+  highlight = {
+    enable = true,
+  },
+}
