fb9fc2f17a3d5a45419fa1ec04e61e519482de73
Sturmfahrt <SturmfahrtDev@proton.me>
2023-12-27 21:32:10 -0500

    Added a few things i have added lazy.nvim as the package manager. using onedark as the theme. added lualine as this makes things look nicer. added nvim tree as netrw is ugly. and have thrown in an options file for neovim options, as well as a keymaps file where custom keymaps are assigned. also added a gitignore.

---
+++ lua/plugins.lua
@@ -0,0 +1,11 @@
+return {
+  "navarasu/onedark.nvim",
+  {
+    'nvim-lualine/lualine.nvim',
+    dependencies = { 'nvim-tree/nvim-web-devicons' },
+  },
+  {
+    'nvim-tree/nvim-tree.lua',
+    dependencies = { 'nvim-tree/nvim-web-devicons' },
+  },
+}
