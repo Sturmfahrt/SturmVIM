235e515a329c602c756bb65356badbbe545ed056
Sturmfahrt <cyberhead81@icloud.com>
2024-01-03 18:45:43 -0500

    Restructure has begun I have moved the started the process of moving plugin stuff to there own lua module. so everything needed to load and configure it will be defined in its own lua module.

--- init.lua
+++ init.lua
@@ -18,8 +18,6 @@ require("lazy").setup("plugins")
 
 require "user.keymaps"
 require "user.options"
-require "user.colorscheme"
-require "user.lualine"
 require "user.nvim-tree"
 require "user.telescope"
 require "user.treesitter"
--- lua/plugins.lua
+++ lua/plugins.lua
@@ -1,9 +1,4 @@
 return {
-  "navarasu/onedark.nvim",
-  {
-    'nvim-lualine/lualine.nvim',
-    dependencies = { 'nvim-tree/nvim-web-devicons' },
-  },
   {
     'nvim-tree/nvim-tree.lua',
     dependencies = { 'nvim-tree/nvim-web-devicons' },
