ee34a657b99e32634fd70de0a4d37e6a88501d48
Sturmfahrt <SturmfahrtDev@proton.me>
2024-01-06 15:55:51 -0500

    Added Some LSP keybinds added some keybinds for things like code actions. code actions will use a telescope ui.

--- lua/plugins/lsp-config.lua
+++ lua/plugins/lsp-config.lua
@@ -18,6 +18,10 @@ return {
     config = function()
       local lspconfig = require("lspconfig")
       lspconfig.lua_ls.setup({})
+
+      vim.keymap.set('n', 'K', vim.lsp.buf.hover, {})
+      vim.keymap.set('n', 'gd', vim.lsp.buf.definition, {})
+      vim.keymap.set({ 'n' }, '<leader>ca', vim.lsp.buf.code_action, {})
     end
   }
 }
