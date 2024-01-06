f8af1a4fa86ff9420e9c7e5b29bbe01017601fe7
Sturmfahrt <cyberhead81@Icloud.com>
2024-01-06 15:55:51 -0500

    Added Some LSP keybinds added some keybinds for things like code actions. code actions will use a telescope ui.

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -1,9 +1,25 @@
 return {
+  {
     'nvim-telescope/telescope.nvim',
     branch = '0.1.x',
     dependencies = { 'nvim-lua/plenary.nvim' },
     name = "telescope",
     config = function()
-      require("telescope").setup{}
+      require("telescope").setup {
+        extensions = {
+          ["ui-select"] = {
+            require("telescope.themes").get_dropdown {
+              -- even more opts
+            }
+          }
+        }
+      }
     end
+  },
+  {
+    "nvim-telescope/telescope-ui-select.nvim",
+    config = function()
+      require("telescope").load_extension("ui-select")
+    end
+  }
 }
