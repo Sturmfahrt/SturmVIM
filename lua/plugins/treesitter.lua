969a554df1a66d5019d6ef53d4597e149da45c57
Sturmfahrt <SturmfahrtDev@proton.me>
2024-01-06 17:51:42 -0500

    added basic autocompletion Now that i have autocomplete working in lua. the main thing left is to get a good DAP implementation. and figure out how to integrate rust into this config. i also actually bothered to fix the formatting so things are more consistent.

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -1,14 +1,14 @@
 return {
-    'nvim-treesitter/nvim-treesitter',
-    config = function()
-      require'nvim-treesitter.configs'.setup {
-        ensure_installed = {
-          "lua",
-        },
-        auto_install = true,
-        highlight = {
-          enable = true,
-        },
-      }
-    end
+	"nvim-treesitter/nvim-treesitter",
+	config = function()
+		require("nvim-treesitter.configs").setup({
+			ensure_installed = {
+				"lua",
+			},
+			auto_install = true,
+			highlight = {
+				enable = true,
+			},
+		})
+	end,
 }
