fd824a6b4f9c4f3bb88bf6ea037f9f8ffac79893
Sturmfahrt <SturmfahrtDev@proton.me>
2024-02-13 19:39:33 -0500

    Switching theme to catppuccin just switched the theme from onedark to catppuccin, as i am going to see what happens if i style my whole setup to follow catppuccin.

---
+++ lua/plugins/catppuccin.lua
@@ -0,0 +1,11 @@
+return {
+  "catppuccin/nvim",
+  lazy = false,
+  name = "catppuccin",
+  priority = 1000,
+  config = function()
+    require("catppuccin").setup({
+      flavor = "frappe"
+    })
+  end
+}
