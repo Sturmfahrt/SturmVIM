51a7ecd853ab845215411d51d12bccc81c913f38
Sturmfahrt <SturmfahrtDev@proton.me>
2024-02-13 19:58:58 -0500

    changed catppuccin to mocha

--- lua/plugins/catppuccin.lua
+++ lua/plugins/catppuccin.lua
@@ -5,7 +5,7 @@ return {
   priority = 1000,
   config = function()
     require("catppuccin").setup({
-      flavour = "frappe",
+      flavour = "mocha",
     })
     vim.cmd.colorscheme "catppuccin"
   end,
