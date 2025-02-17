3a53e13c0155df9b7fde0d5e7827c62fdeadb785
Sturmfahrt <SturmfahrtDev@proton.me>
2025-02-17 17:20:24 -0500

    changing lualine theme

--- lua/plugins/lualine.lua
+++ lua/plugins/lualine.lua
@@ -4,7 +4,7 @@ return {
 	config = function()
 		require("lualine").setup({
 			options = {
-				theme = "onedark",
+				theme = "catppuccin",
 				-- ... the rest of your lualine config
 			},
 		})
